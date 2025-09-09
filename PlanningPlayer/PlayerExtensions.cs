5cd4dad14b1b71706f796805c5e3c62a07015ece
gpolcode <62902968+gpolcode@users.noreply.github.com>
2025-09-09 09:10:09 +0200

    Cache reflection delegates for faster cloning

--- PlanningPlayer/PlayerExtensions.cs
+++ PlanningPlayer/PlayerExtensions.cs
@@ -1,4 +1,6 @@
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Reflection;
 using OGameSim.Entities;
 using OGameSim.Production;
@@ -7,6 +9,9 @@ namespace PlanningPlayer;
 
 public static class PlayerExtensions
 {
+    static readonly FieldInfo PlanetsField = typeof(Player).GetField("_planets", BindingFlags.NonPublic | BindingFlags.Instance)!;
+    static readonly FieldInfo? LastUpdatedField = typeof(Player).GetField("_lastUpdatedAstroLevel", BindingFlags.NonPublic | BindingFlags.Instance);
+
     public static Player DeepClone(this Player player)
     {
         var clone = new Player();
@@ -17,17 +22,15 @@ public static class PlayerExtensions
         CopyAstrophysics(player.Astrophysics, clone.Astrophysics);
         CopyPlasmaTechnology(player.PlasmaTechnology, clone.PlasmaTechnology);
 
-        var planetsField = typeof(Player).GetField("_planets", BindingFlags.NonPublic | BindingFlags.Instance)!;
-        var originalPlanets = (List<Planet>)planetsField.GetValue(player)!;
-        var clonedPlanets = new List<Planet>();
+        var originalPlanets = (List<Planet>)PlanetsField.GetValue(player)!;
+        var clonedPlanets = new List<Planet>(originalPlanets.Count);
         foreach (var planet in originalPlanets)
         {
             clonedPlanets.Add(ClonePlanet(planet));
         }
-        planetsField.SetValue(clone, clonedPlanets);
+        PlanetsField.SetValue(clone, clonedPlanets);
 
-        var lastUpdatedField = typeof(Player).GetField("_lastUpdatedAstroLevel", BindingFlags.NonPublic | BindingFlags.Instance);
-        lastUpdatedField?.SetValue(clone, lastUpdatedField.GetValue(player));
+        LastUpdatedField?.SetValue(clone, LastUpdatedField.GetValue(player));
 
         return clone;
     }
@@ -65,33 +68,74 @@ public static class PlayerExtensions
 
     static T GetProperty<T>(object obj, string name)
     {
-        var prop = obj.GetType().GetProperty(name, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)!;
-        return (T)prop.GetValue(obj)!;
+        var getter = AccessorCache.GetGetter(obj.GetType(), name)!;
+        return (T)getter(obj)!;
     }
 
     static void SetProperty(object obj, string name, object value)
     {
-        var prop = obj.GetType().GetProperty(name, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)!;
-        var setter = prop.GetSetMethod(true)!;
-        setter.Invoke(obj, new[] { value });
+        var setter = AccessorCache.GetSetter(obj.GetType(), name)!;
+        setter(obj, value);
     }
 
     static void CopyProperty(object source, object target, string name)
     {
-        const BindingFlags flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy;
-        var sourceProp = source.GetType().GetProperty(name, flags);
-        var targetProp = target.GetType().GetProperty(name, flags);
-
-        // ignore properties that cannot be copied (missing getter/setter)
-        var getter = sourceProp?.GetGetMethod(true);
-        var setter = targetProp?.GetSetMethod(true);
+        var getter = AccessorCache.GetGetter(source.GetType(), name);
+        var setter = AccessorCache.GetSetter(target.GetType(), name);
         if (getter is null || setter is null)
         {
             return;
         }
 
-        var value = getter.Invoke(source, null);
-        setter.Invoke(target, new[] { value });
+        var value = getter(source);
+        setter(target, value);
+    }
+
+    static class AccessorCache
+    {
+        const BindingFlags Flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy;
+
+        static readonly ConcurrentDictionary<(Type Type, string Name), Func<object, object?>> Getters = new();
+        static readonly ConcurrentDictionary<(Type Type, string Name), Action<object, object?>> Setters = new();
+
+        public static Func<object, object?>? GetGetter(Type type, string name)
+            => Getters.GetOrAdd((type, name), key => BuildGetter(key.Type, key.Name));
+
+        public static Action<object, object?>? GetSetter(Type type, string name)
+            => Setters.GetOrAdd((type, name), key => BuildSetter(key.Type, key.Name));
+
+        static Func<object, object?>? BuildGetter(Type type, string name)
+        {
+            var prop = type.GetProperty(name, Flags);
+            var getter = prop?.GetGetMethod(true);
+            if (prop is null || getter is null)
+            {
+                return null;
+            }
+
+            var objParam = Expression.Parameter(typeof(object), "obj");
+            var castObj = Expression.Convert(objParam, type);
+            var propAccess = Expression.Property(castObj, prop);
+            var castResult = Expression.Convert(propAccess, typeof(object));
+            return Expression.Lambda<Func<object, object?>>(castResult, objParam).Compile();
+        }
+
+        static Action<object, object?>? BuildSetter(Type type, string name)
+        {
+            var prop = type.GetProperty(name, Flags);
+            var setter = prop?.GetSetMethod(true);
+            if (prop is null || setter is null)
+            {
+                return null;
+            }
+
+            var objParam = Expression.Parameter(typeof(object), "obj");
+            var valueParam = Expression.Parameter(typeof(object), "value");
+            var castObj = Expression.Convert(objParam, type);
+            var castValue = Expression.Convert(valueParam, prop.PropertyType);
+            var body = Expression.Assign(Expression.Property(castObj, prop), castValue);
+            return Expression.Lambda<Action<object, object?>>(body, objParam, valueParam).Compile();
+        }
     }
 }
 
