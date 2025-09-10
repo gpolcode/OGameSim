3f45f03a05dc92cbcb1e71d5c7df10cb9a380cba
gpolcode <62902968+gpolcode@users.noreply.github.com>
2025-09-10 15:11:09 +0200

    Use 8000-day horizon in planner demos

--- PlanningPlayer/Program.cs
+++ PlanningPlayer/Program.cs
@@ -2,5 +2,5 @@ using OGameSim.Entities;
 using PlanningPlayer;
 
 var player = new Player();
-var result = Planner.Search(player, 10);
+var result = Planner.Search(player, 8000);
 Console.WriteLine(result);
